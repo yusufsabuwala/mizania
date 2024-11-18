de104e22ab68fabf72517bd4d0566384445d0a4e
yusufes52 <yusufes52@gmail.com>
2024-11-18 20:26:52 +0000

    modified firbase files

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,10 +7,12 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
+    pkgs.python311
+    #pkgs.python312
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
+    pkgs.python311Packages.pip
   ];
 
   # Sets environment variables in the workspace
